132b75e1cc285b2ee0e52cca88fd06f7507b7cfc
Barry Hanna <barryahanna@gmail.com>
2023-07-14 11:58:39 +0100

    Error handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,39 @@ import { useQuery } from '@apollo/client';
 import Persons from './components/Persons';
 import { ALL_PERSONS } from './queries/queries';
 import './App.css';
+import { useState } from 'react';
+import PersonForm from './components/PersonForm';
 
 function App() {
+	const [errorMessage, setErrorMessage] = useState(null);
+
 	const result = useQuery(ALL_PERSONS);
 
 	if (result.loading) {
 		return <p>loading...</p>;
 	}
 
+	const notify = (message) => {
+		setErrorMessage(message);
+		setTimeout(() => {
+			setErrorMessage(null);
+		}, 10000);
+	};
+
 	return (
 		<div>
+			<Notify errorMessage={errorMessage} />
 			<Persons persons={result.data.allPersons} />
+			<PersonForm setError={notify} />
 		</div>
 	);
 }
+
+const Notify = ({ errorMessage }) => {
+	if (!errorMessage) {
+		return null;
+	}
+	return <div style={{ color: 'red' }}>{errorMessage}</div>;
+};
 
 export default App;
